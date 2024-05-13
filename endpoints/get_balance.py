0da73c4678fc261f2f4ecc8400ad7a70fd147616
kaspaclassic <bitmeme@taxi-x.org>
2024-05-13 06:32:27 -0500

    commit

---
+++ endpoints/get_balance.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+from fastapi import Path, HTTPException
+from pydantic import BaseModel
+
+from server import app, kaspad_client
+
+
+class BalanceResponse(BaseModel):
+    address: str = "btm:qp3gdpw70htp934mmp4fm54sewd23hqjxxshvjpqykw96hlk3nxt5qvgjfpm7"
+    balance: int = 38240000000
+
+
+@app.get("/addresses/{gorAddress}/balance", response_model=BalanceResponse, tags=["Gor addresses"])
+async def get_balance_from_kaspa_address(
+        gorAddress: str = Path(
+            description="Gor address as string e.g. btm:qp3gdpw70htp934mmp4fm54sewd23hqjxxshvjpqykw96hlk3nxt5qvgjfpm7",
+            regex="^btm\:[a-z0-9]{61,63}$")):
+    """
+    Get balance for a given gor address
+    """
+    resp = await kaspad_client.request("getBalanceByAddressRequest",
+                                       params={
+                                           "address": gorAddress
+                                       })
+
+    try:
+        resp = resp["getBalanceByAddressResponse"]
+    except KeyError:
+        if "getUtxosByAddressesResponse" in resp and "error" in resp["getUtxosByAddressesResponse"]:
+            raise HTTPException(status_code=400, detail=resp["getUtxosByAddressesResponse"]["error"])
+        else:
+            raise
+
+    try:
+        balance = int(resp["balance"])
+
+    # return 0 if address is ok, but no utxos there
+    except KeyError:
+        balance = 0
+
+    return {
+        "address": gorAddress,
+        "balance": balance
+    }
---
+++ endpoints/get_utxos.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+from typing import List
+
+from fastapi import Path, HTTPException
+from pydantic import BaseModel
+
+from server import app, kaspad_client
+
+
+class OutpointModel(BaseModel):
+    transactionId: str = "ef62efbc2825d3ef9ec1cf9b80506876ac077b64b11a39c8ef5e028415444dc9"
+    index: int = 0
+
+
+class ScriptPublicKeyModel(BaseModel):
+    scriptPublicKey: str = "20c5629ce85f6618cd3ed1ac1c99dc6d3064ed244013555c51385d9efab0d0072fac"
+
+
+class UtxoModel(BaseModel):
+    amount: str = "11501593788",
+    scriptPublicKey: ScriptPublicKeyModel
+    blockDaaScore: str = "18867232"
+
+
+class UtxoResponse(BaseModel):
+    address: str = "btm:qp3gdpw70htp934mmp4fm54sewd23hqjxxshvjpqykw96hlk3nxt5qvgjfpm7"
+    outpoint: OutpointModel
+    utxoEntry: UtxoModel
+
+
+@app.get("/addresses/{gorAddress}/utxos", response_model=List[UtxoResponse], tags=["BTM addresses"])
+async def get_utxos_for_address(gorAddress: str = Path(
+    description="BTM address as string e.g. gor:qqkqkzjvr7zwxxmjxjkmxxdwju9kjs6e9u82uh59z07vgaks6gg62v8707g73",
+    regex="^btm\:[a-z0-9]{61,63}$")):
+    """
+    Lists all open utxo for a given gor address
+    """
+    resp = await kaspad_client.request("getUtxosByAddressesRequest",
+                                       params={
+                                           "addresses": [gorAddress]
+                                       }, timeout=120)
+    try:
+        return (utxo for utxo in resp["getUtxosByAddressesResponse"]["entries"] if utxo["address"] == gorAddress)
+    except KeyError:
+        if "getUtxosByAddressesResponse" in resp and "error" in resp["getUtxosByAddressesResponse"]:
+            raise HTTPException(status_code=400, detail=resp["getUtxosByAddressesResponse"]["error"])
+        else:
+            return []
